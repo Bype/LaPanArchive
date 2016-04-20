27a88ec1d0476f55b32b108be3feb3b5fe04971b
David Olivari <david.olivari@gmail.com>
2016-04-20 14:27:22 +0200

    solved aspect ratio change.

--- data/transfert.py
+++ data/transfert.py
@@ -1,7 +1,13 @@
 import json
 from firebase import firebase
 
-firebase = firebase.FirebaseApplication('https://la-panacee.firebaseio.com/catalogue', None)
+firebase = firebase.FirebaseApplication('https://la-panacee.firebaseio.com', None)
+
+with open("expo.json") as json_file:
+    json_data = json.load(json_file)
+    for aw in json_data:
+      aw.pop("id")
+      firebase.post('/exhibitions', aw)
 
 
 with open("lapanarchive.json") as json_file:
